ec2c9f410f68cd4ffd7a5978ea1f648b783ec065
Ian <Ian.Hinden@gmail.com>
2021-03-11 20:52:12 -0800

    Added abilit to deal damage to enemies

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -6,13 +6,11 @@ public class EnemyPathing : MonoBehaviour
 {
     WaveConfig waveConfig;
     List<Transform> waypoints;
-    float moveSpeed;
     int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = waveConfig.GetMoveSpeed();
         waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
@@ -33,7 +31,7 @@ public class EnemyPathing : MonoBehaviour
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
 
             if (transform.position == targetPosition)
